098c53d6eb760fb29a963767ac1e4c010bd5e883
Walker Randolph Smith <walker@wearesoftwarecraftsmen.com>
2015-08-15 00:30:16 -0400

    Created schema. Created mutations. Created components. Update package.json.

--- build/babelGraphQLPlugin.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-var getBabelGraphQLPlugin = require('babel-relay-plugin');
-var path = require('path');
-
-var SCHEMA_PATH = path.resolve(__dirname, '../data/schema.json');
-
-var schema = JSON.parse(fs.readFileSync(SCHEMA_PATH, 'utf8'));
-
-module.exports = getBabelGraphQLPlugin(schema.data);
--- data/database.js
+++ data/database.js
@@ -1,30 +1,37 @@
-/**
- *  Copyright (c) 2015, Facebook, Inc.
- *  All rights reserved.
- *
- *  This source code is licensed under the BSD-style license found in the
- *  LICENSE file in the root directory of this source tree. An additional grant
- *  of patent rights can be found in the PATENTS file in the same directory.
- */
-
 // Model types
-class User extends Object {}
-class Widget extends Object {}
+class Game extends Object {}
+class HidingSpot extends Object {}
 
 // Mock data
-var viewer = new User();
-viewer.id = '1';
-viewer.name = 'Anonymous';
-var widgets = ['What\'s-it', 'Who\'s-it', 'How\'s-it'].map((name, i) => {
-  var widget = new Widget();
-  widget.name = name;
-  widget.id = `${i}`;
-  return widget;
-});
+var game = new Game();
+game.id = '1';
 
-module.exports = {
-  // Export methods that your schema can use to interact with your database
-  getViewer: () => viewer,
-  getWidget: (id) => widgets.find(w => w.id === id),
-  getWidgets: () => widgets,
+var hidingSpots = [];
+(function() {
+  var hidingSpot;
+  var indexOfSpotWithTreasure = Math.floor(Math.random() * 9);
+  for (var i = 0; i < 9; i++) {
+    hidingSpot = new HidingSpot();
+    hidingSpot.id = `${i}`;
+    hidingSpot.hasTreasure = (i === indexOfSpotWithTreasure);
+    hidingSpot.hasBeenChecked = false;
+    hidingSpots.push(hidingSpot);
+  }
+})();
+
+var turnsRemaining = 3;
+
+export function checkHidingSpotForTreasure(id) {
+  if (hidingSpots.some(hs => hs.hasTreasure && hs.hasBeenChecked)) {
+    return;
+  }
+  turnsRemaining--;
+  var hidingSpot = getHidingSpot(id);
+  hidingSpot.hasBeenChecked = true;
 };
+export function getHidingSpot(id) {
+  return hidingSpots.find(hs => hs.id === id)
+}
+export function getGame() { return game; }
+export function getHidingSpots() { return hidingSpots; }
+export function getTurnsRemaining() { return turnsRemaining; }
